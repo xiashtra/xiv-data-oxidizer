30f498516af1f1fc72400dcf876bfc469d2d93c4
augenfrosch <45207902+augenfrosch@users.noreply.github.com>
2026-03-04 16:22:24 +0100

    Automatically detect available languages

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,33 @@ mod exd_schema;
 mod export;
 mod formatter;
 
-const LANGUAGES: [Language; 4] = [
-    Language::English,
-    Language::German,
-    Language::French,
-    Language::Japanese,
-];
+fn available_languages(ironworks: &Ironworks) -> Vec<Language> {
+    // Wokaround used by boilmaster, ironworks currently doesn't support directly checking if a file exists.
+    struct FileExists;
+    impl ironworks::file::File for FileExists {
+        fn read(_stream: impl ironworks::FileStream) -> Result<Self, ironworks::Error> {
+            Ok(Self)
+        }
+    }
+
+    ironworks
+        .file::<ironworks::file::exh::ExcelHeader>("exd/Item.exh")
+        .expect("Could not read 'exd/Item.exh'")
+        .languages
+        .into_iter()
+        .map(Language::from)
+        .filter(|language| {
+            // Check if the files actually exist. The Global version's `EXcelHeader`s indicate that
+            // all languages are supported, even though the files aren't present besides for EN, DE, FR, and JA.
+            ironworks
+                .file::<FileExists>(&format!(
+                    "exd/Item_0_{}.exd",
+                    export::language_code(language)
+                ))
+                .is_ok()
+        })
+        .collect()
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -29,9 +50,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new(&args[1]);
 
     let ironworks = Ironworks::new().with_resource(SqPack::new(Install::at(path)));
+    let languages = available_languages(&ironworks);
     let mut excel = Excel::new(ironworks);
 
-    for language in LANGUAGES {
+    for language in languages {
         excel.set_default_language(language);
         let sheets = excel.list().expect("Could not retrieve sheet list.");
 
